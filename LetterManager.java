9f3762c886304d55125a1e73deb79b9411de89b9
inderdeep25 <idsk80@gmail.com>
2014-09-03 10:44:48 +0530

    Update LetterManager.java

--- LetterManager.java
+++ LetterManager.java
@@ -6,18 +6,18 @@ public class LetterManager {
 	
 	public ArrayList<LetterData> list=new ArrayList<LetterData>();
 	
-	public int get_Score_Of_Letter(char ch)   //character ch is in the word
+	public float get_Score_Of_Letter(char ch)   //character ch is in the word
 	{
 	     LetterData ob=new LetterData();
-	     int score=ob.get_Score();
+	     float score=ob.get_Score();
 	     return score;
 	}
 	
 	public void Update_Placed_Letter(char ch)
 	{
 		LetterData ob=new LetterData();
-		ob.score-=2;
-		ob.frequency-=1;
+		ob.score*=1.5;
+		ob.frequency*=0.4;
 	}
 
 }
